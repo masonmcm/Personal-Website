6df6458b0f5818217156e8be433ba834c0d60af2
Mason Mathew <masonmc@live.unc.edu>
2019-07-05 00:33:55 -0400

    Adjusted react files for a more responsive gallery

--- src/client/gallery.tsx
+++ src/client/gallery.tsx
@@ -5,13 +5,41 @@ import ReactDOM from "react-dom";
 
 // 2. Import our Data
 import data from "./script/data"
+import { string } from "prop-types";
 
 let main = () => {
-    // TODO:
+
+    window.addEventListener("scroll", function() {
+
+    forms.forEach((form) => { 
+        formsRecord[form] = ((window.scrollY > document.getElementById(form + "-container").offsetTop - 150) &&
+        (window.scrollY < document.getElementById(form + "-container").offsetTop + document.getElementById(form + "-container").offsetHeight - 150) ? true : false);
+    });
+
+    for(let i = 0; i < forms.length; i++) {
+        if(formsRecord[forms[i]] === true){
+            this.document.getElementById(forms[i]).style.fontWeight = "bold";
+            this.document.getElementById(forms[i]).style.fontStyle = "italic";
+        }
+        if(formsRecord[forms[i]] === false){
+            this.document.getElementById(forms[i]).style.fontWeight = "200";
+            this.document.getElementById(forms[i]).style.fontStyle = "normal";
+        }
+    }
+    });
+
     ReactDOM.render(
         <Gallery displays={data} />, 
         document.getElementById("gallery-container"));
 };
+
+const forms = ["2D", "3D", "Photography"];
+
+let formsRecord: Record<string, boolean> = {};
+
+for(let i = 0; i < forms.length; i++) {
+    formsRecord[forms[i]] = false;
+}
 
 
 type Display = {
@@ -19,9 +47,11 @@ type Display = {
     thumbSrc: string,
     src: string, 
     title: string, 
+    relativeUrl: string,
     collab: string,
     date: number, 
     medium: string,
+    form: string,
     size: string
 }
 
@@ -36,10 +66,12 @@ type GalleryProps = {
 
 type GalleryState = {
     selected: Display,
-    showPopUp: boolean
+    showPopUp: boolean, 
+    forms: string[]
 }
 
 type ThumbnailProps = {
+    id: string,
     display: Display,
     selected: boolean,
     onSelect?: (display: Display, showPopUp: boolean) => void,
@@ -51,7 +83,8 @@ class Gallery extends React.Component<GalleryProps, GalleryState> {
 
         this.state = {
             selected: this.props.displays[0],
-            showPopUp: false
+            showPopUp: false,
+            forms: forms
         };
     }
 
@@ -59,12 +92,19 @@ class Gallery extends React.Component<GalleryProps, GalleryState> {
         return <div className="gallery-pop-up-container">
         <PopUp hidden={!this.state.showPopUp} display={this.state.selected}/>
         <div className="gallery">{
-            this.props.displays.map((display) => {
-                return <Thumbnail key={display.src} display={display}
-                selected= {(display === this.state.selected) ? true : false }
-                onSelect= {(display, showPopUp) => this.selectedHandler(display, showPopUp)}/>
-            })
+            this.generateGallery(this.state.forms)
             }</div>
+        <div className="table-container">
+            <div className="table">
+            <ul id="gallery-selector">
+                {forms.map((form) => {
+                    return <li key={form}><a href={"portfolio.html#" + form + "-container"}><p className="gallery-link" id={form} 
+                    onMouseOver={() => this.handleMouseOver(form)} onMouseLeave={() => this.handleMouseOut(form)}>{form}</p></a>
+                    {(form === forms[forms.length - 1]) ? "" : " /"}</li>
+                })}
+            </ul>
+            </div>
+        </div>
         </div>;
     }
 
@@ -75,6 +115,31 @@ class Gallery extends React.Component<GalleryProps, GalleryState> {
         });
     }
 
+    handleMouseOver(id: string): void {
+        document.getElementById(id).style.fontWeight = "bold";
+        document.getElementById(id).style.fontStyle = "italic";
+    }
+
+    handleMouseOut(id: string): void {
+        document.getElementById(id).style.fontWeight = "200";
+        document.getElementById(id).style.fontStyle = "normal";
+    }
+
+    generateGallery(forms: string[]): JSX.Element[] {
+        let elements = [];
+        for(let i = 0; i < forms.length; i++){
+            elements.push(<div id={forms[i] + "-container"} key={forms[i] + "-container"} className="form-container">
+            <h2>{forms[i]}</h2>
+            {this.props.displays.filter((display) => {return display.form === forms[i]}).map((display) => {
+                return <Thumbnail id={display.relativeUrl} key={display.src} display={display}
+                    selected= {(display === this.state.selected) ? true : false }
+                        onSelect= {(display, showPopUp) => this.selectedHandler(display, showPopUp)}/>})}
+                            <img className="page-break gallery-page-break" src="images/svg/page-break.svg"></img></div>
+                        );
+                    
+        }
+        return elements;
+    }
 }
 
 class PopUp extends React.Component<PopUpProps> {
@@ -87,7 +152,7 @@ class PopUp extends React.Component<PopUpProps> {
                     <div className={this.generatePopUpSize()}>
                     <img className="pop-up-image" src={this.props.display.src} alt="image"></img>
                     <div className="pop-up-caption">
-                        <p><em>{this.props.display.title}</em>{(this.props.display.collab === "none") ? "" : ` (Collab with ${this.props.display.collab})`}</p>
+                        <p><em>{this.props.display.relativeUrl.replace(/-/g, " ")}</em>{(this.props.display.collab === "none") ? "" : ` (Collab with ${this.props.display.collab})`}</p>
                         <p>{this.props.display.medium}</p>
                           <p>{this.props.display.date}</p>
                     </div>
@@ -113,7 +178,7 @@ class PopUp extends React.Component<PopUpProps> {
 
 class Thumbnail extends React.Component<ThumbnailProps> {
     render() {
-        return <img className="gallery-image" onMouseDown={() => this.handleMouseEvent(true)} onMouseLeave={() => this.handleMouseEvent(false)} src={this.props.display.thumbSrc} alt={this.props.display.title}></img>;
+        return <img id={this.props.id} className="gallery-image" onMouseDown={() => this.handleMouseEvent(true)} onMouseLeave={() => this.handleMouseEvent(false)} src={this.props.display.thumbSrc} alt={this.props.display.title}></img>;
     }
 
     handleMouseEvent(showPopUp: boolean) {
--- src/client/script/data.ts
+++ src/client/script/data.ts
@@ -4,9 +4,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/color-study_thumb.jpg",
         "src": "./images/jpg/full-size/color-study_full.jpg",
         "title": "Color Wheel Study",
+        "relativeUrl": "Color-Wheel-Study",
         "collab": "none",
         "date": 2017,
         "medium": "Gouache on Bristol board",
+        "form": "2D",
         "size": "wide"
     },
 
@@ -15,9 +17,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/costume_thumb.jpg",
         "src": "./images/jpg/full-size/costume_full.jpg",
         "title": "Outfit for 3D Design",
+        "relativeUrl": "Outfit-for-3D-Design",
         "collab": "none",
         "date": 2016,
         "medium": "Bedsheet, Housepaint, Pantyhose, misc.",
+        "form": "3D",
         "size": "regular"
     },
 
@@ -26,9 +30,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/greer_thumb.jpg",
         "src": "./images/jpg/full-size/greer_full.jpg",
         "title": "Pixel Portrait 4",
+        "relativeUrl": "Pixel-Portrait-4",
         "collab": "Walker Bankson",
         "date": 2016,
         "medium": "Acrylic on canvas",
+        "form": "2D",
         "size": "regular"
     },
 
@@ -37,9 +43,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/jean-shirt_thumb.jpg",
         "src": "./images/jpg/full-size/jean-shirt_full.jpg",
         "title": "Shirt 3",
+        "relativeUrl": "Shirt-3",
         "collab": "none",
         "date": 2016,
         "medium": "Housepaint on jean shirt",
+        "form": "2D",
         "size": "regular"
     },
 
@@ -48,9 +56,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/kanye_thumb.jpg",
         "src": "./images/jpg/full-size/kanye_full.jpg",
         "title": "Pixel Portrait 1",
+        "relativeUrl": "Pixel-Portrait-1",
         "collab": "none",
         "date": 2016,
         "medium": "Acrylic and housepaint on canvas",
+        "form": "2D",
         "size": "regular"
     },
 
@@ -59,9 +69,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/kendall_thumb.jpg",
         "src": "./images/jpg/full-size/kendall_full.jpg",
         "title": "Pixel Portrait 2",
+        "relativeUrl": "Pixel-Portrait-2",
         "collab": "none",
         "date": 2016,
         "medium": "Acrylic and housepaint on canvas",
+        "form": "2D",
         "size": "regular"
     },
 
@@ -70,9 +82,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/lattice-1_thumb.jpg",
         "src": "./images/jpg/full-size/lattice-1_full.jpg",
         "title": "Lattice Study 1",
+        "relativeUrl": "Lattice-Study-1",
         "collab": "none",
         "date": 2018,
         "medium": "Acrylic on Bristol board",
+        "form": "2D",
         "size": "wide"
     },
 
@@ -81,9 +95,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/reliquary_thumb.jpg",
         "src": "./images/jpg/full-size/reliquary_full.jpg",
         "title": "Reliquary for 3D Design",
+        "relativeUrl": "Reliquary-for-3D-Design",
         "collab": "none",
         "date": 2016,
         "medium": "Wood, Ceramic, Acrylic",
+        "form": "3D",
         "size": "regular"
     },
 
@@ -92,9 +108,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/sin-shirt_thumb.jpg",
         "src": "./images/jpg/full-size/sin-shirt_full.jpg",
         "title": "Sin City",
+        "relativeUrl": "Sin-City",
         "collab": "Noah McGee",
         "date": 2016,
         "medium": "Acrylic and housepaint on canvas shirt",
+        "form": "2D",
         "size": "narrow"
     },
 
@@ -103,9 +121,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/sketch-1_thumb.jpg",
         "src": "./images/jpg/full-size/sketch-1_full.jpg",
         "title": "Sketch 1",
+        "relativeUrl": "Sketch-1",
         "collab": "none",
         "date": 2017,
         "medium": "Gouache on paper",
+        "form": "Photography",
         "size": "narrow"
     },
 
@@ -114,9 +134,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/sketch-2_thumb.jpg",
         "src": "./images/jpg/full-size/sketch-2_full.jpg",
         "title": "Sketch 2",
+        "relativeUrl": "Sketch-2",
         "collab": "none",
         "date": 2017,
         "medium": "Gouache on paper",
+        "form": "Photography",
         "size": "narrow"
     },
 
@@ -125,9 +147,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/sketch-3_thumb.jpg",
         "src": "./images/jpg/full-size/sketch-3_full.jpg",
         "title": "Sketch 3",
+        "relativeUrl": "Sketch-3",
         "collab": "none",
         "date": 2017,
         "medium": "Gouache on paper",
+        "form": "Photography",
         "size": "narrow"
     },
 
@@ -136,9 +160,11 @@ export default [
         "thumbSrc": "./images/jpg/thumb/wire-portrait_thumb.jpg",
         "src": "./images/jpg/full-size/wire-portrait_full.jpg",
         "title": "Self Portrait for 3D-Design",
+        "relativeUrl": "Self-Portrait-for-3D-Design",
         "collab": "none",
         "date": 2016,
         "medium": "Metal wire and paper",
+        "form": "3D",
         "size": "regular"
     },
 
