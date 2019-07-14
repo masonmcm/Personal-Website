2d07adfb15dff180449197b84563066d953bd51f
Mason Mathew <masonmc@live.unc.edu>
2019-07-14 19:37:30 -0400

    Added new photos to gallery

--- src/client/landing-page.tsx
+++ src/client/landing-page.tsx
@@ -7,16 +7,6 @@ let main = () => {
     ReactDOM.render(<LandingPage/>, document.getElementById("landing-page-container"));
 };
 
-let load = () => {
-    loaded = true;
-}
-
-let unload = () => {
-    loaded = false;
-}
-
-let loaded = true;
-
 type LandingPageState = {
     chosenColor: string
     pageLoaded: boolean
@@ -48,25 +38,29 @@ class LandingPage extends React.Component<LandingPageProps, LandingPageState>{
                 <div className="nav-grid-container">
                     <Fade left delay={100}><div className="nav-grid">
                         <div className="nav-row nav-row-top">
-                            <div className="grid-item grid-item_portfolio">
-                                <a href="portfolio.html">
+                            <div className="grid-item grid-item_portfolio" onMouseOver={() => this.onHover("plain",  "white")}
+                                onMouseOut={() => this.onHover("white",  "plain")}>
+                                <a href="portfolio">
                                 <img className="grid-image" src="./images/svg/portfolio-button_label.svg" alt="portfolio-button"></img>
                                 </a>
                             </div>
-                        <div className="grid-item grid-item_code">
-                            <a href="code.html">
+                        <div className="grid-item grid-item_code" onMouseOver={()=>this.onHover("plain",  "yellow")}
+                            onMouseOut={()=>this.onHover("yellow",  "plain")}>
+                            <a href="code">
                             <img className="grid-image" src="./images/svg/code-button_label.svg" alt="code-button"></img>
                             </a>
                         </div>
                         </div>
                         <div className="nav-row nav-row-bottom">
-                            <div className="grid-item grid-item_writing">
-                                <a href="writing.html">
+                            <div className="grid-item grid-item_writing" onMouseOver={()=>this.onHover("plain",  "blue")}
+                                onMouseOut={()=>this.onHover("blue",  "plain")}>
+                                <a href="writing">
                                 <img className="grid-image" src="./images/svg/writing-button_label.svg" alt="writing-button"></img>
                                 </a>
                             </div>
-                            <div className="grid-item grid-item_about">
-                                <a href="about.html">
+                            <div className="grid-item grid-item_about" onMouseOver={()=>this.onHover("plain",  "red")}
+                            onMouseOut={()=>this.onHover("red",  "plain")}>
+                                <a href="about">
                                 <img className="grid-image" src="./images/svg/about-button_label.svg" alt="about-button"></img>
                                 </a>    
                             </div>
@@ -75,9 +69,15 @@ class LandingPage extends React.Component<LandingPageProps, LandingPageState>{
                 </div>
             </div>
         }
+
+    onHover(hide: string, show: string) {
+        const hideElement = document.getElementsByClassName(hide)[0] as HTMLElement;
+        const showElement = document.getElementsByClassName(show)[0] as HTMLElement;
+        
+        hideElement.style.display = "none";
+        showElement.style.display = "block";
+    };
 }
 
 
-window.addEventListener("load", load);
-window.addEventListener("onunload", unload);
 window.addEventListener("load", main);
--- src/client/script/data.ts
+++ src/client/script/data.ts
@@ -1,58 +1,20 @@
 export default [
+
     {
         "id": 0,
-        "thumbSrc": "./images/jpg/thumb/color-study_thumb.jpg",
-        "src": "./images/jpg/full-size/color-study_full.jpg",
-        "title": "Color Wheel Study",
-        "relativeUrl": "Color-Wheel-Study",
+        "thumbSrc": "./images/jpg/thumb/riff-raff_thumb.jpg",
+        "src": "./images/jpg/full-size/riff-raff_full.jpg",
+        "title": "Pixel Portrait 1",
+        "relativeUrl": "Pixel-Portrait-1",
         "collab": "none",
-        "date": 2017,
-        "medium": "Gouache on Bristol board",
+        "date": 2015,
+        "medium": "Acrylic on Canvas",
         "form": "2D",
         "size": "wide"
     },
 
     {
         "id": 1,
-        "thumbSrc": "./images/jpg/thumb/costume_thumb.jpg",
-        "src": "./images/jpg/full-size/costume_full.jpg",
-        "title": "Outfit for 3D Design",
-        "relativeUrl": "Outfit-for-3D-Design",
-        "collab": "none",
-        "date": 2016,
-        "medium": "Bedsheet, Housepaint, Pantyhose, misc.",
-        "form": "3D",
-        "size": "regular"
-    },
-
-    {
-        "id": 2,
-        "thumbSrc": "./images/jpg/thumb/greer_thumb.jpg",
-        "src": "./images/jpg/full-size/greer_full.jpg",
-        "title": "Pixel Portrait 4",
-        "relativeUrl": "Pixel-Portrait-4",
-        "collab": "Walker Bankson",
-        "date": 2016,
-        "medium": "Acrylic on canvas",
-        "form": "2D",
-        "size": "regular"
-    },
-
-    {
-        "id": 3,
-        "thumbSrc": "./images/jpg/thumb/jean-shirt_thumb.jpg",
-        "src": "./images/jpg/full-size/jean-shirt_full.jpg",
-        "title": "Shirt 3",
-        "relativeUrl": "Shirt-3",
-        "collab": "none",
-        "date": 2016,
-        "medium": "Housepaint on jean shirt",
-        "form": "2D",
-        "size": "regular"
-    },
-
-    {
-        "id": 4,
         "thumbSrc": "./images/jpg/thumb/kanye_thumb.jpg",
         "src": "./images/jpg/full-size/kanye_full.jpg",
         "title": "Pixel Portrait 1",
@@ -65,7 +27,7 @@ export default [
     },
 
     {
-        "id": 5,
+        "id": 2,
         "thumbSrc": "./images/jpg/thumb/kendall_thumb.jpg",
         "src": "./images/jpg/full-size/kendall_full.jpg",
         "title": "Pixel Portrait 2",
@@ -78,20 +40,20 @@ export default [
     },
 
     {
-        "id": 6,
-        "thumbSrc": "./images/jpg/thumb/lattice-1_thumb.jpg",
-        "src": "./images/jpg/full-size/lattice-1_full.jpg",
-        "title": "Lattice Study 1",
-        "relativeUrl": "Lattice-Study-1",
-        "collab": "none",
-        "date": 2018,
-        "medium": "Acrylic on Bristol board",
+        "id": 3,
+        "thumbSrc": "./images/jpg/thumb/greer_thumb.jpg",
+        "src": "./images/jpg/full-size/greer_full.jpg",
+        "title": "Pixel Portrait 4",
+        "relativeUrl": "Pixel-Portrait-4",
+        "collab": "Walker Bankson",
+        "date": 2016,
+        "medium": "Acrylic on canvas",
         "form": "2D",
-        "size": "wide"
+        "size": "regular"
     },
 
     {
-        "id": 7,
+        "id": 4,
         "thumbSrc": "./images/jpg/thumb/reliquary_thumb.jpg",
         "src": "./images/jpg/full-size/reliquary_full.jpg",
         "title": "Reliquary for 3D Design",
@@ -104,7 +66,113 @@ export default [
     },
 
     {
+        "id": 5,
+        "thumbSrc": "./images/jpg/thumb/lattice-1_thumb.jpg",
+        "src": "./images/jpg/full-size/lattice-1_full.jpg",
+        "title": "Lattice Study 1",
+        "relativeUrl": "Lattice-Study-1",
+        "collab": "none",
+        "date": 2018,
+        "medium": "Acrylic on Bristol board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+    {
+        "id": 6,
+        "thumbSrc": "./images/jpg/thumb/lattice-2_thumb.jpg",
+        "src": "./images/jpg/full-size/lattice-2_full.jpg",
+        "title": "Lattice Study 2",
+        "relativeUrl": "Lattice-Study-2",
+        "collab": "none",
+        "date": 2018,
+        "medium": "Acrylic on Bristol Board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+
+    {
+        "id": 7,
+        "thumbSrc": "./images/jpg/thumb/lattice-3_thumb.jpg",
+        "src": "./images/jpg/full-size/lattice-3_full.jpg",
+        "title": "Lattice Study 3",
+        "relativeUrl": "Lattice-Study-3",
+        "collab": "none",
+        "date": 2019,
+        "medium": "Acrylic on Bristol Board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+    {
         "id": 8,
+        "thumbSrc": "./images/jpg/thumb/lattice-4_thumb.jpg",
+        "src": "./images/jpg/full-size/lattice-4_full.jpg",
+        "title": "Lattice Study 4",
+        "relativeUrl": "Lattice-Study-4",
+        "collab": "none",
+        "date": 2019,
+        "medium": "Acrylic on Bristol Board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+    {
+        "id": 9,
+        "thumbSrc": "./images/jpg/thumb/lattice-5_thumb.jpg",
+        "src": "./images/jpg/full-size/lattice-5_full.jpg",
+        "title": "Lattice Study 5",
+        "relativeUrl": "Lattice-Study-5",
+        "collab": "none",
+        "date": 2019,
+        "medium": "Acrylic on Bristol Board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+    {
+        "id": 10,
+        "thumbSrc": "./images/jpg/thumb/color-study_thumb.jpg",
+        "src": "./images/jpg/full-size/color-study_full.jpg",
+        "title": "Color Wheel Study",
+        "relativeUrl": "Color-Wheel-Study",
+        "collab": "none",
+        "date": 2017,
+        "medium": "Gouache on Bristol board",
+        "form": "2D",
+        "size": "wide"
+    },
+
+    {
+        "id": 11,
+        "thumbSrc": "./images/jpg/thumb/costume_thumb.jpg",
+        "src": "./images/jpg/full-size/costume_full.jpg",
+        "title": "Outfit for 3D Design",
+        "relativeUrl": "Outfit-for-3D-Design",
+        "collab": "none",
+        "date": 2016,
+        "medium": "Bedsheet, Housepaint, Pantyhose, misc.",
+        "form": "3D",
+        "size": "regular"
+    },
+
+
+    {
+        "id": 12,
+        "thumbSrc": "./images/jpg/thumb/jean-shirt_thumb.jpg",
+        "src": "./images/jpg/full-size/jean-shirt_full.jpg",
+        "title": "Shirt 3",
+        "relativeUrl": "Shirt-3",
+        "collab": "none",
+        "date": 2016,
+        "medium": "Housepaint on jean shirt",
+        "form": "2D",
+        "size": "regular"
+    },
+
+    {
+        "id": 13,
         "thumbSrc": "./images/jpg/thumb/sin-shirt_thumb.jpg",
         "src": "./images/jpg/full-size/sin-shirt_full.jpg",
         "title": "Sin City",
@@ -115,46 +183,4 @@ export default [
         "form": "2D",
         "size": "narrow"
     },
-
-    {
-        "id": 9,
-        "thumbSrc": "./images/jpg/thumb/sketch-1_thumb.jpg",
-        "src": "./images/jpg/full-size/sketch-1_full.jpg",
-        "title": "Sketch 1",
-        "relativeUrl": "Sketch-1",
-        "collab": "none",
-        "date": 2017,
-        "medium": "Gouache on paper",
-        "form": "Photography",
-        "size": "narrow"
-    },
-
-    {
-        "id": 10,
-        "thumbSrc": "./images/jpg/thumb/sketch-2_thumb.jpg",
-        "src": "./images/jpg/full-size/sketch-2_full.jpg",
-        "title": "Sketch 2",
-        "relativeUrl": "Sketch-2",
-        "collab": "none",
-        "date": 2017,
-        "medium": "Gouache on paper",
-        "form": "Photography",
-        "size": "narrow"
-    },
-
-    {
-        "id": 11,
-        "thumbSrc": "./images/jpg/thumb/sketch-3_thumb.jpg",
-        "src": "./images/jpg/full-size/sketch-3_full.jpg",
-        "title": "Sketch 3",
-        "relativeUrl": "Sketch-3",
-        "collab": "none",
-        "date": 2017,
-        "medium": "Gouache on paper",
-        "form": "Photography",
-        "size": "narrow"
-    },
-
-
-
 ]
